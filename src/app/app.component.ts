bbef1233cfc62108a1582829a47a4ad86c2f91ff
Ralph Mensah <ralphmensah123@gmail.com>
2022-06-15 20:05:06 +0000

    trial with reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, FormBuilder,Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactiveForms';
+export class AppComponent implements OnInit{
+
+// using the form group
+  // form = new FormGroup({
+  //   lastName : new FormControl(''),
+  //   firstName : new FormControl(''),
+  //   email : new FormControl(''),
+  //   gender: new FormControl(''),
+  //   address: new FormGroup({
+  //     state : new FormControl(''),
+  //     city: new FormControl('')
+  //   })
+  // })
+
+  form = this.builder.group({
+    lastName: ['',Validators.required],
+    firstName:['', Validators.required],
+    email:['', Validators.required],
+    gender:['',Validators.required],
+    address: this.builder.group({
+      state: [''],
+      city:['']
+    }),
+    aliases: this.builder.array([this.builder.control('')])
+  })
+
+  constructor(private builder : FormBuilder){
+  }
+
+  ngOnInit(): void {
+    // console.log(this.form);
+  }
+
+  get aliases(){
+    return this.form.get('aliases') as FormArray;
+  }
+
+  addAlias(){
+    this.aliases.push(this.builder.control(''))
+    // console.log(this.aliases.length);
+  }
+
+  removeAlias(){
+    this.aliases.removeAt(this.aliases.length-1)
+  }
+
+  onSubmit(){
+    // submit form
+    console.warn(this.form.value)
+    this.form.reset();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
